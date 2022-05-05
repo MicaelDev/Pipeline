ccaddff325be884e5448319730b47f23b34371f9
Micael Quilião Gerson <micaelquiliao20@gmail.com>
2022-05-05 16:37:45 -0300

    Criação do Projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pipeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
